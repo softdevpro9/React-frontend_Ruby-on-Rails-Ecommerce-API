ca28a875525465fd09558d7da308461f332da915
softdevpro9 <shearinmichael0@gmail.com>
2020-04-01 19:57:00 -0500

    Deleted a bunch of default startup crap, added a simple card component, installed Material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Card from 'UI/card'
+
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/products.json").then(response => {
+      response.json().then(data => {
+        setProducts(data);
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
-
+      {products.map(product => (
+        <div key={product.title + product.id}>
+        <h3 >{product.title}</h3>
+        {product.collections.map(col => (
+         <p key={col.title + col.id}>{ col.title} </p>
+        ))}</div>
+      ))
+      }
     </div>
   );
 }
